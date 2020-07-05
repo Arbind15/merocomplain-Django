b7768fe5008be3d28c5b12dcb5995caf943daa75
Arbind15 <mehtaarbind67@gmail.com>
2020-07-05 14:49:26 +0545

    initial

---
+++ mainsite/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+import csv, json, itertools, re
+import os
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.conf import settings
+
+
+def home(request):
+
+    return render(request,'mainsite/home.html')
+
+def login(request):
+
+    return render(request,'mainsite/login.html')
